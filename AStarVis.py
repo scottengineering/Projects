063e42d9a15ef6b498fdcdbd971884222776b69b
Scott <scott90@cs.uw.edu>
2021-09-05 17:55:50 -0400

    halfway

--- AStarVis.py
+++ AStarVis.py
@@ -21,6 +21,7 @@ class Node:
         self.width = width
         self.y = pos[0] * width
         self.x = pos[1] * width
+        self.barrier = False
 
     def getColor(self):
         self.color = red
@@ -33,7 +34,10 @@ class Node:
         return self.pos
 
     def isBarrier(self):
-        return self.color == black
+        return self.barrier
+
+    def setBarrier(self, bar):
+        self.barrier = bar
 
     def drawRect(self, window):
         pygame.draw.rect(window, self.color, (self.x, self.y, self.width, self.width))
@@ -87,59 +91,29 @@ class Visualizer:
     def getNode(self, pos):
         return self.board[pos[0]][pos[1]]
 
+    def getNodeColor(self, pos):
+        return self.board[pos[0]][pos[1]].getColor()
+
     def clickPos(self, pos):
         row = pos[0] // self.nodeWidth
         col = pos[1] // self.nodeWidth
 
-        return (row, col)
+        return (col, row)
 
     def changeColor(self, pos, newColor):
-        self.board[pos[1]][pos[0]].setColor(newColor)
+        self.board[pos[0]][pos[1]].setColor(newColor)
 
-# def makeBoard(rows, nodeWidth):
-#     retBoard = []
-#     for i in range(rows):
-#         rowList = []
-#         for j in range(rows):
-#             newNode = Node((i,j), white, nodeWidth)
-#             rowList.append(newNode)
-#
-#         retBoard.append(rowList)
-#
-#     return retBoard
-#
-# def getNode(pos, board):
-#     return board[pos[0]][pos[1]]
-#
-# def drawBoard(window, rows, nodeWidth):
-#     for i in range(rows):
-#         y = i * nodeWidth
-#         pygame.draw.line(window, black, (0, y), (size, y))
-#
-#     for j in range(rows):
-#         x = j * nodeWidth
-#         pygame.draw.line(window, black, (x, 0), (x, size))
-#
-# def draw(window, board, rows, nodeWidth):
-#     for row in board:
-#         for n in row:
-#             n.drawRect(window)
-#
-#     drawBoard(window, rows, nodeWidth)
-#     pygame.display.update()
-#
-# def clickPos(pos, nodeWidth):
-#     row = pos[0] // nodeWidth
-#     col = pos[1] // nodeWidth
-#
-#     return (row, col)
+    def setBarrier(self, pos, bar):
+        self.board[pos[0]][pos[1]].setBarrier(bar)
+
 
 def main(window):
     rows = 50
     vis = Visualizer(rows)
-    start = None
-    end = None
-
+    start = False
+    end = False
+    startPos = (-1, -1)
+    endPos = (-1, -1)
     run = True
     started = False
     while run:
@@ -153,7 +127,24 @@ def main(window):
 
             if pygame.mouse.get_pressed()[0]:
                 pos = vis.clickPos(pygame.mouse.get_pos())
-                vis.changeColor(pos, red)
+                if not start:
+                    start = True
+                    vis.changeColor(pos, red)
+                    startPos = pos
+                elif not end:
+                    end = True
+                    vis.changeColor(pos, blue)
+                    endPos = pos
+                elif start and pos == startPos:
+                    start = False
+                    vis.changeColor(pos, white)
+                elif end and pos == endPos:
+                    end = False
+                    vis.changeColor(pos, white)
+            elif pygame.mouse.get_pressed()[2]:
+                pos = vis.clickPos(pygame.mouse.get_pos())
+                vis.changeColor(pos, black)
+                vis.setBarrier(pos, True)
 
 
     pygame.quit()
